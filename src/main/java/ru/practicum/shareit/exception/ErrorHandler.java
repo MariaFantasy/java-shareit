522f3df3d9bdc4f419ab6269e0fab6e5da6ae51e
MariaFantasy <peggy-sju@ya.ru>
2024-12-29 02:19:45 +0300

    Обработка ошибок

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(final NotFoundException e) {
+        return new ErrorResponse(
+                "Объект не найден.",
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleAlreadyExistError(final AlreadyExistException e) {
+        return new ErrorResponse(
+                "Объект уже существует.",
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationError(final ValidationException e) {
+        return new ErrorResponse(
+                "Объект не прошел валидацию.",
+                e.getMessage()
+        );
+    }
+}
