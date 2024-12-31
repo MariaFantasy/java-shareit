d58c00adbb5ae1777ccf8d3cc41c58465150ec78
MariaFantasy <peggy-sju@ya.ru>
2025-01-01 02:32:37 +0300

    Доработка маппера для UserDto

--- src/main/java/ru/practicum/shareit/user/mapper/UserDtoMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserDtoMapper.java
@@ -1,17 +1,27 @@
 package ru.practicum.shareit.user.mapper;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserRequestDto;
+import ru.practicum.shareit.user.dto.UserResponseDto;
 import ru.practicum.shareit.user.model.User;
 
 @Component
 public class UserDtoMapper {
-    public User mapFromDto(UserDto userDto) {
+    public User mapFromDto(UserRequestDto userRequestDto) {
         final User user = new User(
                 null,
-                userDto.getName(),
-                userDto.getEmail()
+                userRequestDto.getName(),
+                userRequestDto.getEmail()
         );
         return user;
+    }
+
+    public UserResponseDto mapToResponseDto(User user) {
+        final UserResponseDto userDto = new UserResponseDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
+        return userDto;
     }
 }
