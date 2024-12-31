295863df5663607e0377126c8e128b9983388b15
MariaFantasy <peggy-sju@ya.ru>
2025-01-01 02:37:30 +0300

    Доработка маппера для ItemDto

--- src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
@@ -1,20 +1,31 @@
 package ru.practicum.shareit.item.mapper;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
+import ru.practicum.shareit.item.dto.ItemRequestDto;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.dto.UserResponseDto;
 
 @Component
 public class ItemDtoMapper {
-    public Item mapFromDto(User user, ItemDto itemDto) {
+    public Item mapFromDto(UserResponseDto user, ItemRequestDto itemRequestDto) {
         final Item item = new Item(
                 null,
                 user,
-                itemDto.getName(),
-                itemDto.getDescription(),
-                itemDto.getAvailable()
+                itemRequestDto.getName(),
+                itemRequestDto.getDescription(),
+                itemRequestDto.getAvailable()
         );
         return item;
+    }
+
+    public ItemResponseDto mapToResponseDto(Item item) {
+        final ItemResponseDto itemDto = new ItemResponseDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable()
+        );
+        return itemDto;
     }
 }
